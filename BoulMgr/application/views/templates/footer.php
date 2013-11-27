1d71282659e74a2e4b35bfb3f1d1971fea471d27
Anthony Ruhier <anthony.ruhier@gmail.com>
2013-11-27 10:35:15 +0100

    Add footer in templates

---
+++ BoulMgr/application/views/templates/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <p>Boulangerie Manager a été créé via
+    <a href="http://ellislab.com/codeigniter">CodeIgniter</a><br>
+    Le CSS du projet <a href="http://foundation.zurb.com/">Foundation</a>
+    a été utilisé</p>
+
+<!-- TODO : Modifier adresse de conctact-->
+    <p>Pour tout contact ou problème : <a href="#">Contact</a></p>
+</footer>
+
+<script src="public/javascript/jquery.js"></script>
+<script src="public/javascript/foundation.min.js"></script>
+<script src="public/javascript/foundation.dropdown.js"></script>
+</body>
