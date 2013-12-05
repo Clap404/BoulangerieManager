6c9a8827d6934ca808b39586ea0141b411c52dd0
Anthony Ruhier <anthony.ruhier@gmail.com>
2013-12-05 16:30:58 +0100

    Add link to Matprem_detail in Matprem view Matprem view modified to be more clean

--- BoulMgr/application/views/stocks/matprem_v.php
+++ BoulMgr/application/views/stocks/matprem_v.php
@@ -3,35 +3,39 @@
 <?php
 if(count($matprem) != 0)
 {
-    echo('
-        <table>
-            <tr>
-                <td colspan="2">LA PHOTO</td>
-            <tr />
-            <tr>
-                <td colspan="2">'.$matprem[0]['nom_matiere_premiere'].'</td>
-            <tr />
-            <tr>
-                <td>'.$matprem[0]['disponibilite_matiere_premiere'].' pièces</td>
-                <td>- +</td>
-            </tr>
-        </table>
+    ?>
+    <table>
+        <tr>
+            <td colspan="2">LA PHOTO</td>
+        <tr />
+        <tr>
+            <td colspan="2"><?= $matprem[0]['nom_matiere_premiere'] ?></td>
+        <tr />
+        <tr>
+            <td><?= $matprem[0]['disponibilite_matiere_premiere'] ?> pièces</td>
+            <td>- +</td>
+        </tr>
+    </table>
 
-        <p></p>
-        <table>
-            <tr>
-                <td>ID</td>
-                <td>Matière première</td>
-                <td>Quantité disponible</td>
-            </tr>');
+    <p></p>
+    <table>
+        <tr>
+            <td>ID</td>
+            <td>Matière première</td>
+            <td>Quantité disponible</td>
+        </tr>
+    <?php
 
     foreach($matprem as $result) {
-        echo('
-            <tr>
-                <td>'.$result['id_matiere_premiere'].'</td>
-                <td>'.$result['nom_matiere_premiere'].'</td>
-                <td>'.$result['disponibilite_matiere_premiere'].' pièces</td>
-            </tr>');
+        $matprem_detailed_adr = site_url("stocks/matprem/detail/".$result['id_matiere_premiere']);
+        // TODO : Change the cursor in pointer with "cursor: pointer" for the <tr>
+        ?>
+            <tr onclick="document.location='<?= $matprem_detailed_adr ?>';">
+                <td><?= $result['id_matiere_premiere'] ?></td>
+                <td><?= anchor($matprem_detailed_adr, $result['nom_matiere_premiere']) ?></td>
+                <td><?= $result['disponibilite_matiere_premiere'] ?> pièces</td>
+            </tr>
+        <?php
     }
     echo('</table>');
 }
@@ -40,4 +44,6 @@ else
 {
     echo('Aucune matière première existante.');
 }
-?>
+
+/* End of file matprem_v.php */
+/* Location: ./application/view/stocks/matprem_v.php */
