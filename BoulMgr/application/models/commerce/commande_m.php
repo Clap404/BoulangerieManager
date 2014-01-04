b0886df8bd49784b7f6196934ac8786ae19b7f3b
aquassaut <jeremy.autran@gmail.com>
2014-01-04 17:43:30 +0100

    gestion des commandes

---
+++ BoulMgr/application/models/commerce/commande_m.php
@@ -0,0 +1,109 @@
+<?php
+class Commande_m extends CI_Model {
+
+    function __construct(){
+        // Call the Model constructor
+        parent::__construct();
+        $this->load->database();
+    }
+
+    function liste_commandes() {
+        $sql = "SELECT *,
+                    (case
+                        when date(date_livraison) < date('now')
+                        then 'Y' else 'N' end) as finished
+                FROM commande
+                    NATURAL LEFT JOIN client
+                    NATURAL LEFT JOIN commande_contient_produit
+                GROUP BY commande.id_commande
+                ORDER BY commande.date_livraison DESC;";
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+
+    function liste_single_commande($id_commande) {
+        $sql = "SELECT *
+                FROM commande
+                    NATURAL JOIN adresse
+                    NATURAL JOIN client
+                WHERE id_commande = $id_commande";
+        $query = $this->db->query($sql);
+        return $query->row_array();
+    }
+
+    function liste_produits_pour($id_commande) {
+        $sql = "SELECT p.*, id_produit, quantite_produit_commande
+                FROM commande
+                    NATURAL JOIN commande_contient_produit
+                    NATURAL JOIN produit p
+                WHERE id_commande = $id_commande";
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+
+    function get_client_for_commande($id_commande) {
+        $sql = "SELECT id_client
+                FROM commande
+                WHERE id_commande = $id_commande ;";
+        $query = $this->db->query($sql);
+        return $query->row_array();
+    }
+
+    function get_adresse_for_commande($id_commande) {
+        $sql = "SELECT id_adresse
+                FROM commande
+                WHERE id_commande = $id_commande ;";
+        $query = $this->db->query($sql);
+        return $query->row_array();
+    }
+
+    function get_date_for_commande($id_commande) {
+        $sql = "SELECT date_livraison
+                FROM commande
+                WHERE id_commande = $id_commande ;";
+        $query = $this->db->query($sql);
+        return $query->row_array();
+    }
+
+    function delete_commande($id) {
+        $sql = "DELETE FROM commande_contient_produit
+                WHERE id_commande = $id";
+        $this->db->query($sql);
+        $sql = "DELETE FROM commande WHERE id_commande = $id";
+        $this->db->query($sql);
+    }
+
+    function modif_commande($id_commande, $commande) {
+        $sql = "DELETE FROM commande_contient_produit
+                WHERE id_commande = $id_commande";
+        $this->db->query($sql);
+        $sql = "UPDATE commande
+                SET id_client = $commande->client,
+                    id_adresse = $commande->adresse,
+                    date_livraison = $commande->date
+                WHERE id_commande = $id_commande ;";
+        $this->db->query($sql);
+        foreach($commande->commande as $prod) {
+            if ($prod->quantite !== "0" || $prod->quantite !== 0) {
+                $sql ="INSERT INTO commande_contient_produit
+                    VALUES ($id_commande, $prod->id, $prod->quantite);";
+                $this->db->query($sql);
+            }
+        }
+    }
+
+    function ajoute_commande($commande) {
+        $sql = "INSERT INTO commande
+                VALUES (null, 0, datetime('now'), datetime('$commande->date'),
+                        $commande->client, $commande->adresse);";
+        $res = $this->db->query($sql);
+        $newId = $this->db->insert_id();
+        foreach($commande->commande as $prod) {
+            if ($prod->quantite !== "0" || $prod->quantite !== 0) {
+                $sql = "INSERT INTO commande_contient_produit
+                        VALUES ($newId, $prod->id, $prod->quantite);";
+                $this->db->query($sql);
+            }
+        }
+    }
+}
---
+++ BoulMgr/assets/js/commande.js
@@ -0,0 +1,202 @@
+var commande = [];
+
+function populateTable() {
+    var products = document.querySelectorAll(".product-active");
+    var totQuant = document.querySelector(".summary tfoot tr .total-quantity");
+    var totPrix = document.querySelector(".summary tfoot tr .total-price");
+    var tbody = document.createElement("tbody");
+    var tr, td, i;
+    commande = [];
+    if (products.length === 0) {
+        tr = document.createElement("tr");
+        
+        td = document.createElement("td");
+        td.className = "item-name";
+        td.innerHTML = "Pas de produit sélectionné";
+        tr.appendChild(td);
+
+        for (i = 0; i < 3; i ++) {
+            td = document.createElement("td");
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    for (i = 0; i < products.length; i ++) {
+        tr = document.createElement("tr");
+        var prod = {};
+        prod.id = products[i].getAttribute("name");
+        prod.nom = products[i].querySelector(".product-name").innerHTML;
+        prod.prix = parseFloat(products[i].querySelector(".prix").getAttribute("value"), 10).toFixed(2);
+        prod.quantite = parseInt(products[i].querySelector(".qty").value, 10);
+        commande.push(prod);
+
+
+        td = document.createElement("td");
+        td.className = "item-name";
+        td.innerHTML = prod.nom;
+        tr.appendChild(td);
+
+        td = document.createElement("td");
+        td.className = "unit-price";
+        td.innerHTML = prod.prix;
+        tr.appendChild(td);
+        
+        td = document.createElement("td");
+        td.className = "quantity";
+        td.innerHTML = prod.quantite;
+        tr.appendChild(td);
+
+        td = document.createElement("td");
+        td.className = "subtotal-price";
+        td.innerHTML = (prod.quantite * prod.prix).toFixed(2);
+        tr.appendChild(td);
+
+        tbody.appendChild(tr);
+    }
+    var total = commande.reduce(function(acc, x) {
+        quantite = acc.quantite + x.quantite;
+        prix = acc.prix + x.quantite * x.prix;
+        return { quantite : quantite, prix : prix };
+    }, { quantite : 0, prix : 0});
+    document.querySelector(".summary tbody").innerHTML = tbody.innerHTML;
+    document.querySelector(".summary tfoot tr .total-quantity").innerHTML = total.quantite.toFixed(2);
+    document.querySelector(".summary tfoot tr .total-price").innerHTML = total.prix.toFixed(2);
+}
+
+function checkIfNewActive() {
+    var products = document.querySelectorAll(".product, .product-active");
+    for (var i = 0; i < products.length; i ++) {
+        var val = products[i].children[5].value;
+        if (val !== parseInt(val, 10).toString()) {
+            products[i].children[5].value = 0;
+            products[i].className = "product";
+        } else if (val === "0") {
+            products[i].className = "product";
+        } else {
+            products[i].className = "product-active";
+        }
+    }
+    populateTable();
+}
+
+function resetAll() {
+    var products = document.querySelectorAll(".product, .product-active");
+    for (var i = 0; i < products.length; i ++) {
+        products[i].querySelector(".qty").value = 0;
+    }
+    checkIfNewActive();
+}
+
+function addQtyToProduct(qty, product) {
+    var input = document.querySelector("#product-" + product + " .qty");
+    var val = parseInt(input.value, 10);
+    if (typeof val === 'undefined' || typeof val === 'string' ||
+        val === null || Number.isNaN(val)) {
+        val = 0;
+    }
+    val += qty;
+    if (val < 0) {
+        val = 0;
+    } else if (val >= 999) {
+        //999 super arbitraire
+        val = 999;
+    }
+    input.value = val;
+    checkIfNewActive();
+}
+
+function saveTicket() {
+    //manipulation de la date
+    var datetime = document.querySelector("#datepicker").value.split(" ");
+    var date = datetime[0].split("/").reverse().join("-");
+    var datetime = date + " " + datetime[1] + ":00";
+    
+    //TODO valider la date, le client, l'adresse
+
+    var toSend = {
+        commande : commande,
+        client : document.querySelector("#client").value,
+        adresse : document.querySelector("#adresse").value,
+        date : datetime
+    };
+    toSend = JSON.stringify(toSend);
+    console.log(toSend);
+    var url = rootURL + "/commerce/commande/save/" + id_commande;
+    $.ajax({
+        type : "POST",
+        url : url,
+        data : toSend,
+        contentType : "application/json; charset=utf8",
+        success : function(data) {
+            if (data === "OK") {
+                document.location = rootURL + "/commerce/commande/";
+            } else {
+                alert("Erreur côté serveur !");
+                console.log(data);
+            }
+        },
+        failure : function(err) {
+            alert("Impossible d'envoyer la requête au serveur pour le moment !");
+        }
+    });
+}
+
+window.onload = function() {
+    populateTable();
+    $("#datepicker").datetimepicker({
+        lang:'fr',
+        i18n:{
+            fr:{
+                months:[
+                'Janvier','Fevrier','Mars','Avril',
+                'Mai','Juin','Juillet','Août',
+                'Septembre','Octobre','Novembre','Decembre',
+                ],
+                dayOfWeek:[
+                "Di", "Lu", "Ma", "Me", 
+                "Je", "Ve", "Sa",
+                ]
+            }
+        },
+        minDate:0,
+        minTime:'6:00',
+        maxTime:'19:00',
+        mask:true,
+        timepicker:true,
+        format:'d/m/Y H:i',
+        dayOfWeekStart: 1
+    });
+};
+
+function updateAdresses(toSelect) {
+    toSelect = toSelect || -1; 
+    var client = document.querySelector("#client").value;
+    var url = rootURL + "/commerce/commande/getadresse/" + client;
+    $.ajax({
+        type : "GET",
+        url : url,
+        success : function(data) {
+            var adresses = JSON.parse(data);
+            var select = document.querySelector("#adresse");
+            select.innerHTML = "";
+            for (var i = 0; i < adresses.length; i += 1) {
+                var opt = document.createElement("option");
+                opt.value = adresses[i]["id_adresse"];
+
+                if (parseInt(opt, 10) === toSelect) {
+                    opt.selected = true;
+                }
+                opt.innerHTML = adresses[i]["numero_voie_adresse"] + ", " +
+                                adresses[i]["nom_type_voie"] + " " +
+                                adresses[i]["nom_voie_adresse"] + " (" +
+                                adresses[i]["code_postal"] + ", " +
+                                adresses[i]["nom_ville"] + ")";
+                select.appendChild(opt);
+            }
+        },
+        failure : function(err) {
+            alert("Erreur côté serveur !");
+            console.log(err)
+        }
+    });
+}
