cbd3a99d83bcb4837dd0ab9b2f078bbf3099f586
Arnaud Colin <colin.arnaud9@gmail.com>
2014-01-07 19:46:51 +0100

    Update stats_m.php

--- BoulMgr/application/models/stats_m.php
+++ BoulMgr/application/models/stats_m.php
@@ -8,7 +8,7 @@ Class Stats_m extends CI_model {
     $this->load->database();
     }
 
-  function somme_vente_produit(){
+  function somme_vente_produit_annee(){
         $sql= " select nom_produit, sum(total_produit_vente) as somme_produit
                 from
                 (  select id_produit, nom_produit, 
@@ -16,6 +16,24 @@ Class Stats_m extends CI_model {
                 FROM vente_comprend_produit
                 NATURAL JOIN produit 
                 NATURAL JOIN vente 
+                where date(date_vente) > '2014-01-01'
+                )
+                group by id_produit;";
+        $query = $this->db->query($sql);
+        return $query->result_array();
+    }
+    
+    function somme_vente_produit_semaine(){
+        $date_deb = strtotime("-1 week");
+        $date_deb = date('Y/m/d', $date_deb);
+        $sql= " select nom_produit, sum(total_produit_vente) as somme_produit
+                from
+                (  select id_produit, nom_produit, 
+                (quantite_produit_vente * prix_produit) as total_produit_vente 
+                FROM vente_comprend_produit
+                NATURAL JOIN produit 
+                NATURAL JOIN vente  
+                where date_vente > '".$date_deb."'
                 )
                 group by id_produit;";
         $query = $this->db->query($sql);
