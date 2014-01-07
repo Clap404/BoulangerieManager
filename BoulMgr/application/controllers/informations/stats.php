937a4eefbd4b06718a2dffe1ab64aed5885dc25d
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-01-07 23:55:19 +0100

    Stats modified A LOT

--- BoulMgr/application/controllers/informations/stats.php
+++ BoulMgr/application/controllers/informations/stats.php
@@ -1,40 +1,36 @@
 <?php
 class Stats extends CI_Controller {
-   
+
     function __construct()
     {
-            parent::__construct();
-            $this->load->model('stats_m','stats');
-            $this->load->helper('url');
-            $this->load->library('table');
-            date_default_timezone_set("Europe/Paris"); 
+        parent::__construct();
+        $this->load->model('stocks/produits_model','model_produits');
+        $this->load->helper('url');
+        $this->load->library('table');
+        date_default_timezone_set("Europe/Paris");
     }
 
-   function index()
-   {
-         $data['title'] = 'Statistiques';
-         $data['total'] = $this->stats->somme_vente_produit_annee();
-         $total = 0;
-         foreach($data['total'] as $soustot)
-         {
-             $total += $soustot["somme_produit"];
-         }
-         $data['grand_total'] = $total;
+    function index()
+    {
+        $data['title'] = 'Statistiques';
+        $data['total'] = $this->model_produits->somme_vente_produit_annee();
+        $total = 0;
+        foreach($data['total'] as $soustot)
+        {
+            $total += $soustot["somme_produit"];
+        }
+        $data['grand_total'] = $total;
 
-         
+        $data['total2'] = $this->model_produits->somme_vente_produit_semaine();
+        $total2 = 0;
+        foreach($data['total2'] as $soustot)
+        {
+            $total2 += $soustot["somme_produit"];
+        }
+        $data['grand_total2'] = $total2;
 
-         $data['total2'] = $this->stats->somme_vente_produit_semaine();
-         $total2 = 0;
-         foreach($data['total2'] as $soustot)
-         {
-             $total2 += $soustot["somme_produit"];
-         }
-         $data['grand_total2'] = $total2;
-
-        
-
-         $this->load->view('templates/header', $data);
-         $this->load->view('informations/stats_v', $data);
-         $this->load->view('templates/footer');
-   }
+        $this->load->view('templates/header', $data);
+        $this->load->view('informations/stats_v', $data);
+        $this->load->view('templates/footer');
+    }
 }
--- BoulMgr/application/views/informations/stats_v.php
+++ BoulMgr/application/views/informations/stats_v.php
@@ -1,34 +1,43 @@
 <h2>Statistiques</h2>
 
+<div id="chartdiv" style="height:400px;width:900px; margin-left: auto; margin-right: auto;"></div>
+
+<table id='menu'><thead>
+    <tr>
+        <th><a href="#" onclick="drawPie(infos_per_week, 'Répartition des ventes cette semaine');">Semaine</a></th>
+        <th><a href="#" onclick="drawPie(infos_per_year, 'Répartition des ventes cette année');">Année courante</a></th>
+    </tr>
+</thead></table>
+
+<dl class="accordion" data-accordion>
+    <dd>
+        <a href="#panel1">Tableau des ventes de la semaine</a>
+        <div id="panel1" class="content">
 <?php
-
-	echo '<hr>';
-	$this->table->set_caption("Ventes depuis le début de l'année");
-	$this->table->set_heading("Produit", "Total des ventes", "% des ventes");
-	foreach ($total as $line) 
+	$this->table->set_heading("Produit", "Total des ventes (en unités)", "% des ventes");
+	foreach ($total2 as $line)
 	{
 		$this->table->add_row(
 		$line["nom_produit"],
-		$line["somme_produit"]." €", 
-		((number_format($line["somme_produit"]/$grand_total,2))*100)." %");
-	}
-	
-	$this->table->add_row("Total", $grand_total." €", "100 %");
-	echo $this->table->generate(); 
-
-
-	echo '<br><hr><br>';
-
-	$this->table->set_caption("Ventes depuis le début de la semaine");
-	$this->table->set_heading("Produit", "Total des ventes", "% des ventes");
-	foreach ($total2 as $line) 
-	{
-		$this->table->add_row(
-		$line["nom_produit"],
-		$line["somme_produit"]." €", 
+		$line["somme_produit"],
 		((number_format($line["somme_produit"]/$grand_total2,2))*100)." %");
 	}
-	
-	$this->table->add_row("Total", $grand_total2." €", "100 %");
-	echo $this->table->generate(); 
+
+	$this->table->add_row("Total", $grand_total2, "100 %");
+	echo $this->table->generate();
 ?>
+        </div>
+    </dd>
+</dl>
+
+<span id="json_per_year" style="display: none;"><?= json_encode($total) ?></span>
+<span id="json_per_week" style="display: none;"><?= json_encode($total2) ?></span>
+<span id="json_total_per_year" style="display: none;"><?= json_encode($grand_total2) ?></span>
+<span id="json_total_per_week" style="display: none;"><?= json_encode($grand_total2) ?></span>
+
+<link rel="stylesheet" type="text/css" href="<?= base_url("/assets/js/jqplot/jquery.jqplot.css") ?>" />
+<script defer language="javascript" type="text/javascript" src="<?= base_url("/assets/js/jqplot/jquery.jqplot.min.js") ?>"></script>
+<script defer language="javascript" type="text/javascript" src="<?= base_url("/assets/js/jqplot/excanvas.js") ?>"></script>
+<script defer type="text/javascript" src="<?= base_url("/assets/js/jqplot/plugins/jqplot.pieRenderer.min.js") ?>"></script>
+<script defer type="text/javascript" src="<?= base_url("/assets/js/jqplot/plugins/jqplot.donutRenderer.min.js") ?>"></script>
+<script defer src="<?= base_url("/assets/js/informations/stats.js") ?>"></script>
