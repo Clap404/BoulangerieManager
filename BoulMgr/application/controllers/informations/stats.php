e35f8c1d902036ec58009de6c13665dfeacf7cd0
Arnaud Colin <colin.arnaud9@gmail.com>
2014-01-07 19:46:16 +0100

    Update stats.php

--- BoulMgr/application/controllers/informations/stats.php
+++ BoulMgr/application/controllers/informations/stats.php
@@ -10,17 +10,31 @@ class Stats extends CI_Controller {
             date_default_timezone_set("Europe/Paris"); 
     }
 
-    function index() {
-    $data['total'] = $this->stats->somme_vente_produit();
-    $data['title'] = 'Statistiques';
-    $total = 0; 
-    foreach($data['total'] as $soustot)
-    {
-        $total += $soustot["somme_produit"]; 
-    }
-    $data['grand_total'] = $total; 
-    $this->load->view('templates/header', $data);
-    $this->load->view('informations/stats_v', $data);
-    $this->load->view('templates/footer');
-    }
+   function index()
+   {
+         $data['title'] = 'Statistiques';
+         $data['total'] = $this->stats->somme_vente_produit_annee();
+         $total = 0;
+         foreach($data['total'] as $soustot)
+         {
+             $total += $soustot["somme_produit"];
+         }
+         $data['grand_total'] = $total;
+
+         
+
+         $data['total2'] = $this->stats->somme_vente_produit_semaine();
+         $total2 = 0;
+         foreach($data['total2'] as $soustot)
+         {
+             $total2 += $soustot["somme_produit"];
+         }
+         $data['grand_total2'] = $total2;
+
+        
+
+         $this->load->view('templates/header', $data);
+         $this->load->view('stats_v', $data);
+         $this->load->view('templates/footer');
+   }
 }
