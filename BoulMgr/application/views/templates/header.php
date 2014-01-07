37fb2cf13e0f89ee2d85d02a0bdb891ac838feb2
Arnaud Colin <colin.arnaud9@gmail.com>
2014-01-07 16:16:27 +0100

    Update header.php

--- BoulMgr/application/views/templates/header.php
+++ BoulMgr/application/views/templates/header.php
@@ -40,7 +40,7 @@
 
                         <ul class="dropdown">
                             <li>
-                                <a href="#">Statistiques</a>
+                                <a href="<?=site_url("stats")?>">Statistiques</a>
                             </li>
 
                             <li>
