d60d0648c631a9e10d971f41d9d0b1f8ed1681b8
Arnaud Colin <colin.arnaud9@gmail.com>
2014-01-07 19:45:30 +0100

    Update stats_v.php

--- BoulMgr/application/views/informations/stats_v.php
+++ BoulMgr/application/views/informations/stats_v.php
@@ -2,7 +2,9 @@
 
 <?php
 
-	$this->table->set_heading("Produit", "Total des Ventes", "% des ventes");
+	echo '<hr>';
+	$this->table->set_caption("Ventes depuis le début de l'année");
+	$this->table->set_heading("Produit", "Total des ventes", "% des ventes");
 	foreach ($total as $line) 
 	{
 		$this->table->add_row(
@@ -11,7 +13,22 @@
 		((number_format($line["somme_produit"]/$grand_total,2))*100)." %");
 	}
 	
-	$this->table->add_row("Total", $grand_total." €", "");
+	$this->table->add_row("Total", $grand_total." €", "100 %");
+	echo $this->table->generate(); 
 
+
+	echo '<br><hr><br>';
+
+	$this->table->set_caption("Ventes depuis le début de la semaine");
+	$this->table->set_heading("Produit", "Total des ventes", "% des ventes");
+	foreach ($total2 as $line) 
+	{
+		$this->table->add_row(
+		$line["nom_produit"],
+		$line["somme_produit"]." €", 
+		((number_format($line["somme_produit"]/$grand_total2,2))*100)." %");
+	}
+	
+	$this->table->add_row("Total", $grand_total2." €", "100 %");
 	echo $this->table->generate(); 
 ?>
