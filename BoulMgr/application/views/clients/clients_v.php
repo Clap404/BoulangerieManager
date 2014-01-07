c3a6fa23350b090fd6e53e4a2a76c93b92828a41
Clap404 <cl.clap@laposte.net>
2014-01-07 18:11:30 +0100

    bug totaux

--- BoulMgr/application/views/clients/clients_v.php
+++ BoulMgr/application/views/clients/clients_v.php
@@ -1,21 +1,28 @@
 <h2>Clients</h2>
 
 <?php
-    $this->table->set_heading("ID", "Nom du client", "Ville", "Numéro de téléphone", "Montant des commandes", "Fiche détaillée");
+    $this->table->set_heading("ID", "Nom du client", "Ville", "Numéro de téléphone", "Commandes", "Achats", "Total", "Fiche détaillée");
 
 
     foreach ($clients as $value) {
-        if(empty($value->total))
-            $total = 0;
+        if(empty($value->total_commande))
+            $total_commande = 0;
         else
-            $total = $value->total;
+            $total_commande = $value->total_commande;
+
+        if(empty($value->total_vente))
+            $total_vente = 0;
+        else
+            $total_vente = $value->total_vente;
         
         $this->table->add_row(
             $value->id_client,
             $value->nom_client." ".$value->prenom_client,
             $value->nom_ville,
             $value->numero_telephone,
-            $total." €",
+            $total_commande." €",
+            $total_vente." €",
+            $total_vente + $total_commande." €",
             anchor(array("clients", "profil", $value->id_client), "<button class='smallbutton'>Profil</button>")
         );
     }
